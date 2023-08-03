35dd7c3b1a70a32942c1b429417ff52eea6d2fd5
Bracchi-R <bracchipro@gmail.com>
2023-08-03 12:05:30 +0200

    init Springboot

---
+++ zingcollect-springboot/src/test/java/com/example/zingcollectspringboot/ZingcollectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zingcollectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZingcollectSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
