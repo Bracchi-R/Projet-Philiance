becd6fbb07a6d5d583a94454a853b1db114142f1
Bracchi-R <bracchipro@gmail.com>
2023-08-04 15:45:47 +0200

    Ajouts model Springboot

---
+++ zingcollect-springboot/src/main/java/com/example/zingcollectspringboot/model/User.java
@@ -0,0 +1,41 @@
+package com.example.zingcollectspringboot.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "admin", nullable = false)
+    private Boolean admin = false;
+
+    @Column(name = "nom", nullable = false, length = 256)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false, length = 256)
+    private String prenom;
+
+    @Column(name = "tel")
+    private Integer tel;
+
+    @Column(name = "mail", nullable = false)
+    private String mail;
+
+    @Column(name = "mdp", nullable = false, length = 20)
+    private String mdp;
+
+    @OneToMany(mappedBy = "idUser")
+    private Set<Collection> collections = new LinkedHashSet<>();
+
+}
