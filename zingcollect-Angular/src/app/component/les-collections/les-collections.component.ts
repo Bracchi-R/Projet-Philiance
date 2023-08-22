6c33555b16fddb25e3cf176044513af69d3a7769
Bracchi-R <bracchipro@gmail.com>
2023-08-22 10:56:59 +0200

    retour

--- zingcollect-Angular/src/app/component/les-collections/les-collections.component.ts
+++ zingcollect-Angular/src/app/component/les-collections/les-collections.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-les-collections',
   templateUrl: './les-collections.component.html',
   styleUrls: ['./les-collections.component.css']
 })
+
 export class LesCollectionsComponent {
 
 }
