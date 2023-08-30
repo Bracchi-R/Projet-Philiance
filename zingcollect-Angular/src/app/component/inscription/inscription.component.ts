a148bdfd75c613571c3215ba62294dd4ea0d7042
Bracchi-R <bracchipro@gmail.com>
2023-08-30 15:53:58 +0200

    add user

--- zingcollect-Angular/src/app/component/inscription/inscription.component.ts
+++ zingcollect-Angular/src/app/component/inscription/inscription.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/userService/user.service';
 import { User } from 'src/model/user';
 
@@ -9,17 +8,34 @@ import { User } from 'src/model/user';
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css'],
 })
-export class InscriptionComponent {
-  public userForm: FormGroup | undefined;
-  public user: User | undefined;
+export class InscriptionComponent implements OnInit {
+  
+  public userForm! : FormGroup;
 
-  constructor(
-    private userService: UserService,
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute
+  constructor(private userService: UserService, private formBuilder: FormBuilder
   ) {}
 
-
+  ngOnInit(): void {
+      this.userForm = this.formBuilder.group({
+    nom : this.formBuilder.control(""),
+    prenom : this.formBuilder.control(""),
+    tel : this.formBuilder.control(""),
+    mail : this.formBuilder.control(""),
+    mdp : this.formBuilder.control(""),
+      })
+  }
   
+  ajouterUser(){
+    let user : User = this.userForm.value;
+    
+    this.userService.addUser(user).subscribe({
+      next: data =>{
+        console.log(data);
+      },
+      error : err => {
+        console.error(err);
+      }
+    })
+  }
  
 }
